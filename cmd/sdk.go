4fac82fcdbf608c15a7d17e8dd3d998311a75c09
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-10-09 19:49:53 +0200

    set base url

--- cmd/sdk.go
+++ cmd/sdk.go
@@ -12,12 +12,16 @@ type SdkClient struct {
 }
 
 func (sdkClient *SdkClient) GetClient() *sdk.Client {
+	var tokenStore = sdkgen.MemoryTokenStore{}
+
 	credentials := sdkgen.OAuth2{
 		ClientId:     sdkClient.ClientId,
 		ClientSecret: sdkClient.ClientSecret,
 		TokenUrl:     "https://api.sdkgen.app/authorization/token",
+		TokenStore:   tokenStore,
 	}
-	client, err := sdk.NewClient("", credentials)
+
+	client, err := sdk.NewClient("https://api.sdkgen.app", credentials)
 	if err != nil {
 		log.Fatal(err)
 	}
