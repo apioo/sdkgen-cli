37499fc3e804cba2a7db977d513447c8ae96c9b9
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-10-10 15:57:52 +0200

    add php integration

---
+++ integration/php/main.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__ . '/TestTag.php';
+require_once __DIR__ . '/Error.php';
+require_once __DIR__ . '/ErrorException.php';
+require_once __DIR__ . '/HelloWorld.php';
+require_once __DIR__ . '/Message.php';
+require_once __DIR__ . '/Todo.php';
+require_once __DIR__ . '/Todos.php';
+require_once __DIR__ . '/Client.php';
+
+$credentials = new \Sdkgen\Client\Credentials\HttpBearer('foo');
+$client = new Client('http://localhost', $credentials);
+
+assertGetHello($client);
+assertGetEntries($client);
+assertInsert($client);
+assertThrowException($client);
+
+function assertGetHello(Client $client): void
+{
+    $message = $client->test()->getHello();
+
+    if ($message->getMessage() != 'Hello World!') {
+        throw new RuntimeException('Test assertGetHello failed: Message, got: ' . $message->getMessage());
+    }
+}
+
+function assertGetEntries(Client $client): void
+{
+    $todos = $client->test()->getEntries(0, 16);
+
+    if ($todos->getTotalResults() != 4) {
+        throw new RuntimeException("Test assertGetEntries failed: TotalResults, got: " . $todos->getTotalResults());
+    }
+
+    if ($todos->getStartIndex() != 0) {
+        throw new RuntimeException("Test assertGetEntries failed: StartIndex, got: " . $todos->getStartIndex());
+    }
+
+    if ($todos->getItemsPerPage() != 16) {
+        throw new RuntimeException("Test assertGetEntries failed: ItemsPerPage, got: " . $todos->getItemsPerPage());
+    }
+
+    if (count($todos->getEntry()) != 2) {
+        throw new RuntimeException("Test assertGetEntries failed: Entry, got: " . count($todos->getEntry()));
+    }
+
+    if ($todos->getEntry()[0]->getTitle() != 'foo') {
+        throw new RuntimeException("Test assertGetEntries failed: Entry.0.Title, got: " . $todos->getEntry()[0]->getTitle());
+    }
+
+    if ($todos->getEntry()[1]->getTitle() != "bar") {
+        throw new RuntimeException("Test assertGetEntries failed: Entry.1.Title, got: " . $todos->getEntry()[1]->getTitle());
+    }
+}
+
+function assertInsert(Client $client): void
+{
+    $payload = new Todo();
+    $payload->setTitle("baz");
+
+    $message = $client->test()->insert($payload);
+
+    if (!$message->getSuccess()) {
+        throw new RuntimeException("Test assertInsert failed: Success, got: " . $message->getSuccess());
+    }
+
+    if ($message->getMessage() != "Successful") {
+        throw new RuntimeException("Test assertInsert failed: Message, got: " . $message->getMessage());
+    }
+}
+
+function assertThrowException(Client $client): void
+{
+    try {
+        $client->test()->throwException();
+
+        throw new RuntimeException("Test assertThrowException failed: Expected an error");
+    } catch (ErrorException $e) {
+        if ($e->getPayload()->getMessage() != "Error") {
+            throw new RuntimeException("Test assertThrowException failed: Error message does not match, got: " . $e->getPayload()->getMessage());
+        }
+    }
+}
+
