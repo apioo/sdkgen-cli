687b082fba55da543631ff9d2ba9782c5530d765
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-11-07 22:38:10 +0100

    check whether client id and secret is available

--- cmd/sdk.go
+++ cmd/sdk.go
@@ -25,6 +25,14 @@ func (sdkClient *SdkClient) GetClient() *sdk.Client {
 		sdkClient.ClientSecret = os.Getenv("SDKGEN_CLIENT_SECRET")
 	}
 
+	if sdkClient.ClientId == "" {
+		log.Fatal("No client id provided, please provide a client id either through the --client-id flag or environment variable SDKGEN_CLIENT_ID")
+	}
+
+	if sdkClient.ClientSecret == "" {
+		log.Fatal("No client secret provided, please provide a client secret either through the --client-secret flag or environment variable SDKGEN_CLIENT_SECRET")
+	}
+
 	credentials := sdkgen.OAuth2{
 		ClientId:     sdkClient.ClientId,
 		ClientSecret: sdkClient.ClientSecret,
