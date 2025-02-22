2eec4f935d7df68a1f822f32bdb4383cf568d82c
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-02-22 19:10:08 +0100

    add tests

--- integration/python/main.py
+++ integration/python/main.py
@@ -8,29 +8,29 @@ def assert_get_hello(client: Client):
     message = client.test().get_hello()
 
     if message.message != 'Hello World!':
-        raise Exception('Test assertGetHello failed: Message, got: ' + message.message)
+        raise Exception('Test assert_get_hello failed: Message, got: ' + message.message)
 
 
 def assert_get_entries(client: Client):
     todos = client.test().get_entries(0, 16)
 
     if todos.total_results != 4:
-        raise Exception("Test assertGetEntries failed: TotalResults, got: " + str(todos.total_results))
+        raise Exception("Test assert_get_entries failed: TotalResults, got: " + str(todos.total_results))
 
     if todos.start_index != 0:
-        raise Exception("Test assertGetEntries failed: StartIndex, got: " + str(todos.start_index))
+        raise Exception("Test assert_get_entries failed: StartIndex, got: " + str(todos.start_index))
 
     if todos.items_per_page != 16:
-        raise Exception("Test assertGetEntries failed: ItemsPerPage, got: " + str(todos.items_per_page))
+        raise Exception("Test assert_get_entries failed: ItemsPerPage, got: " + str(todos.items_per_page))
 
     if len(todos.entry) != 2:
-        raise Exception("Test assertGetEntries failed: Entry, got: " + str(len(todos.entry)))
+        raise Exception("Test assert_get_entries failed: Entry, got: " + str(len(todos.entry)))
 
     if todos.entry[0].title != 'foo':
-        raise Exception("Test assertGetEntries failed: Entry.0.Title, got: " + todos.entry[0].title)
+        raise Exception("Test assert_get_entries failed: Entry.0.Title, got: " + todos.entry[0].title)
 
     if todos.entry[1].title != "bar":
-        raise Exception("Test assertGetEntries failed: Entry.1.Title, got: " + todos.entry[1].title)
+        raise Exception("Test assert_get_entries failed: Entry.1.Title, got: " + todos.entry[1].title)
 
 
 def assert_insert(client: Client):
@@ -40,20 +40,69 @@ def assert_insert(client: Client):
     message = client.test().insert(payload)
 
     if not message.success:
-        raise Exception("Test assertInsert failed: Success, got: " + str(message.success))
+        raise Exception("Test assert_insert failed: Success, got: " + str(message.success))
 
     if message.message != "Successful":
-        raise Exception("Test assertInsert failed: Message, got: " + message.message)
+        raise Exception("Test assert_insert failed: Message, got: " + message.message)
 
 
 def assert_throw_exception(client: Client):
     try:
         client.test().throw_exception()
 
-        raise Exception("Test assertThrowException failed: Expected an error")
+        raise Exception("Test assert_throw_exception failed: Expected an error")
     except ErrorException as e:
         if e.get_payload().message != "Error":
-            raise Exception("Test assertThrowException failed: Error message does not match, got: " + e.payload.message)
+            raise Exception("Test assert_throw_exception failed: Error message does not match, got: " + e.payload.message)
+
+
+def assert_binary(client: Client):
+    payload = bytes([0x66, 0x6F, 0x6F, 0x62, 0x61, 0x72])
+
+    response = client.test().binary(payload)
+
+    if payload != response:
+        raise Exception("Test assert_binary failed")
+
+
+def assert_form(client: Client):
+    payload = {
+        "foo": "bar"
+    }
+
+    response = client.test().form(payload)
+
+    if payload != response:
+        raise Exception("Test assert_form failed")
+
+
+def assert_json(client: Client):
+    payload = {
+        "foo": "bar"
+    }
+
+    response = client.test().json(payload)
+
+    if payload != response:
+        raise Exception("Test assert_json failed")
+
+
+def assert_text(client: Client):
+    payload = "foobar"
+
+    response = client.test().text(payload)
+
+    if payload != response:
+        raise Exception("Test assert_text failed")
+
+
+def assert_xml(client: Client):
+    payload = "<foo>bar</foo>"
+
+    response = client.test().xml(payload)
+
+    if payload != response:
+        raise Exception("Test assert_xml failed")
 
 
 credentials = Anonymous()
@@ -63,3 +112,8 @@ assert_get_hello(client)
 assert_get_entries(client)
 assert_insert(client)
 assert_throw_exception(client)
+assert_binary(client)
+assert_form(client)
+assert_json(client)
+assert_text(client)
+assert_xml(client)
