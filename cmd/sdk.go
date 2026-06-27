227d20502664d0fe5c2e753eda25e9966b77c01b
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-06-27 18:16:18 +0200

    use anonymous if not client id or secret provided

--- cmd/sdk.go
+++ cmd/sdk.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
-	"github.com/apioo/sdkgen-cli/sdk"
-	"github.com/apioo/sdkgen-go/v2"
 	"log"
 	"os"
+
+	"github.com/apioo/sdkgen-cli/sdk"
+	"github.com/apioo/sdkgen-go/v2"
 )
 
 type SdkClient struct {
@@ -26,19 +27,16 @@ func (sdkClient *SdkClient) GetClient() *sdk.Client {
 		sdkClient.ClientSecret = os.Getenv("SDKGEN_CLIENT_SECRET")
 	}
 
-	if sdkClient.ClientId == "" {
-		log.Fatal("No client id provided, please provide a client id either through the --client-id flag or environment variable SDKGEN_CLIENT_ID")
-	}
-
-	if sdkClient.ClientSecret == "" {
-		log.Fatal("No client secret provided, please provide a client secret either through the --client-secret flag or environment variable SDKGEN_CLIENT_SECRET")
-	}
-
-	credentials := sdkgen.OAuth2{
-		ClientId:     sdkClient.ClientId,
-		ClientSecret: sdkClient.ClientSecret,
-		TokenUrl:     "https://api.sdkgen.app/authorization/token",
-		TokenStore:   tokenStore,
+	var credentials sdkgen.CredentialsInterface
+	if sdkClient.ClientId != "" && sdkClient.ClientSecret != "" {
+		credentials = sdkgen.OAuth2{
+			ClientId:     sdkClient.ClientId,
+			ClientSecret: sdkClient.ClientSecret,
+			TokenUrl:     "https://api.sdkgen.app/authorization/token",
+			TokenStore:   tokenStore,
+		}
+	} else {
+		credentials = sdkgen.Anonymous{}
 	}
 
 	client, err := sdk.NewClient("https://api.sdkgen.app", credentials)
